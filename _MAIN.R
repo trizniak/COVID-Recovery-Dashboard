c11a1bec2fd5486a1e63e7f6c3e0a33630999d66
trizniak <b.micu@yahoo.com>
2021-08-26 17:15:04 +0200

    ...

--- _MAIN.R
+++ _MAIN.R
@@ -2,8 +2,8 @@
 # ---- *** COVID Recovery Dashboard *** ----
 # https://ec.europa.eu/eurostat/cache/recovery-dashboard/
 
-update.input=FALSE # update input data files (estat, ameco, silc)? TRUE/FALSE
-dashboard=FALSE # generate dashboard? TRUE/FALSE
+update.input=TRUE # update input data files (estat, ameco, silc)? TRUE/FALSE
+dashboard=TRUE # generate dashboard? TRUE/FALSE
 
 
 # ---- START ----
@@ -13,10 +13,11 @@ source("./OUTILS/CONFIG/SETUP.R")
 if (update.input) source("./OUTILS/FUNS/DATA.R")
 
 # ---- OUTPUT ----
-{if (update.input | dashboard)
+if (update.input | dashboard) {
   rmarkdown::render(here("OUTILS","BLOX","REPORT.Rmd"),
                     output_file="European Statistical Recovery Dashboard.html",
                     output_dir=here("OUTPUT"),
                     intermediates_dir=here("T E M P"),
                     quiet=TRUE,
-                    clean=TRUE)}
+                    clean=TRUE)
+  }
