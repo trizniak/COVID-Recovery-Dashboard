2a9632aac99d9fa6ee29739090972a3bfabf18c0
trizniak <b.micu@yahoo.com>
2021-12-20 01:43:14 +0100

    ...

--- _MAIN.R
+++ _MAIN.R
@@ -8,6 +8,10 @@ dashboard=TRUE # generate dashboard? TRUE/FALSE
 
 # ---- START ----
 source("./OUTILS/CONFIG/SETUP.R")
+  # pandemic start : 15 March 2020
+  # https://ec.europa.eu/info/live-work-travel-eu/coronavirus-response/timeline-eu-action_en
+  # https://en.wikipedia.org/wiki/COVID-19_pandemic_in_Europe
+start.pandemic=lubridate::ymd("2020-03-15")
 
 # ---- DATA ----
 if (update.input) source("./OUTILS/FUNS/DATA.R")
